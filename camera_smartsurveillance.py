b24a35cae94c42395ee31cf00e358305282433c8
Viktor Dimitrov <32460963+viktordimitrov@users.noreply.github.com>
2017-10-30 12:03:36 -0400

    Update camera_smartsurveillance.py

--- camera_smartsurveillance.py
+++ camera_smartsurveillance.py
@@ -12,7 +12,7 @@ PAGE="""\
 </head>
 <body>
 <center><h1>Smart Surveillance</h1></center>
-<center><img src="stream.mjpg" width="640" height="480"></center>
+<center><img src="stream.mjpg" width="1280" height="720"></center>
 </body>
 </html>
 """
@@ -77,7 +77,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+with picamera.PiCamera(resolution='1280x720', framerate=24) as camera:
     output = StreamingOutput()
     camera.start_recording(output, format='mjpeg')
     try:
