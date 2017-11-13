164ece304120566af5d99a35c263308af641c1ba
Viktor Dimitrov <32460963+viktordimitrov@users.noreply.github.com>
2017-11-13 13:45:32 -0500

    clean up

---
+++ src/MockObject.java
@@ -0,0 +1,27 @@
+/* MockObject class replicates the behavior of:
+ * the Android Application, Server, Camera, Motor and Ultrasonic Sensor
+ * 
+ */
+public class MockObject {
+
+	class testApp{
+		
+	}
+	
+	class testServer{
+		
+	}
+	
+	class testCamera{
+		
+	}
+	
+	class testMotor{
+		
+	}
+	
+	class testSensor{
+		
+	}
+	
+}
