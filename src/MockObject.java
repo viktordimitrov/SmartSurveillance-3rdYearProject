dad329e69b63248c9f8273d5f660e107bf0738ae
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2017-11-16 21:49:58 -0500

    update

--- src/MockObject.java
+++ src/MockObject.java
@@ -2,8 +2,21 @@
  * the Android Application, Server, Camera, Motor and Ultrasonic Sensor
  * 
  */
+
+import static org.easymock.EasyMock.*;
+import org.junit.*;
+
 public class MockObject {
 
+	
+	String message = "Hello World";	
+	   MessageUtil messageUtil = new MessageUtil(message);
+
+	   @Test
+	   public void testPrintMessage() {	  
+	      assertEquals(message,messageUtil.printMessage());
+	   }
+	  
 	class testApp{
 		
 	}
