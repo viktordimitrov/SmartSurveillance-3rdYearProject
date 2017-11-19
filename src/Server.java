84dabbeffb58e050e0f378bfdeae5c72cba09996
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2017-11-19 15:48:35 -0500

    update

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,19 @@
-// methods for server tests
-public class Server {
+import junit.framework.*;
 
-	public void boolean isUserAdded{
-		
-	}
+// methods for server tests
+public class ServerTest extends TestCase {
+	  protected int value1, value2;
+	   
+	   // assigning the values
+	   protected void setUp(){
+	      value1 = 3;
+	      value2 = 3;
+	   }
+
+	   // test method to add two values
+	   public void testAdd(){
+	      double result = value1 + value2;
+	      assertTrue(result == 6);
+	   }
+	
 }
