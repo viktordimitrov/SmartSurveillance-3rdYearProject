bfbfc805a21ed9262c0428697266fad8299002f0
dailinluo <32460791+dailinluo@users.noreply.github.com>
2017-11-19 16:52:44 -0500

    update

--- src/Android.java
+++ src/Android.java
@@ -1,10 +1,50 @@
 import junit.framework.*;
 
-public class Android {
-	public void testWebview(){
+public class Android extends TestCase{
 	
-		assertTrue("This will succeed.", true);
-	}
+	private static final String VALID_URL = "http://www.google.com";
+	private static final String INVALID_URL = "http://invalid.url.doesnotexist987.com";
+	private static final long TIMEOUT = 5000; 
+	private Enlighten_Webview_Main mActivity;
+	private WebView mWeb;
+	private WebViewClient mMockWebViewClient;
+	
+	public final void testLoadValidUrl() {
+        assertLoadUrl(VALID_URL);
+        assertFalse(mMockWebViewClient, mError);
+    }
+
+    public final void testLoadInvalidUrl() {
+        assertLoadUrl(INVALID_URL);
+        assertTrue(mMockWebViewClient.mError);
+    }
+
+    private void assertLoadUrl(String url) {
+        mWeb.loadUrl(url);
+        sleep();
+
+            //Added to hopefully let webview load all the way
+        getInstrumentation().waitForIdleSync();
+
+        assertTrue(!(mWeb.getProgress() < 100));            
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(TIMEOUT);
+        } catch (InterruptedException e) {
+            fail("Unexpected timeout");
+        }
+    }
+    public class MockWebViewClient extends WebViewClient {
+        boolean mError;
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                String description, String failingUrl) {
+            mError = true;
+        }
+    }
 	
 	public void testVideo(){
 		assertTrue("This will succeed.", true);
