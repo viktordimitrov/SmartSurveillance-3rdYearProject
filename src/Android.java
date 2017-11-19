ddab28dfd313b4965f8e8a20bb2b0c09b261e7a4
dailinluo <32460791+dailinluo@users.noreply.github.com>
2017-11-19 16:36:39 -0500

    update

--- src/Android.java
+++ src/Android.java
@@ -1,11 +1,15 @@
+import junit.framework.*;
 
 public class Android {
 	public void testWebview(){
 	
+		assertTrue("This will succeed.", true);
 	}
 	
 	public void testVideo(){
-		
+		assertTrue("This will succeed.", true);
 	}
+
+	
 
 }
