8a0c3da4c2c6d32b48a22c6a2720f95fffe02ea8
bbbrbit <32461090+bbbrbit@users.noreply.github.com>
2017-11-20 13:20:14 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+INT1 = 11
+INT2 = 12
+INT3 = 13
+INT4 = 15
+
+GPIO.setup(INT1,GPIO.OUT)
+GPIO.setup(INT2,GPIO.OUT)
+GPIO.setup(INT3,GPIO.OUT)
+GPIO.setup(INT4,GPIO.OUT)
+
+GPIO.output(INT1,GPIO.HIGH)
+GPIO.output(INT2,GPIO.LOW)
+GPIO.output(INT3,GPIO.HIGH)
+GPIO.output(INT4,GPIO.LOW)
+
+time.sleep(2)
+GPIO.cleanup()
