b651083016321eabebcf7b139369dc694eca66f6
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2017-11-26 21:53:01 -0500

    update

--- src/sampleDatabase.java
+++ src/sampleDatabase.java
@@ -11,6 +11,18 @@ public class sampleDatabase {
    static final String UserInfo = null;
    static boolean tableExists;
    
+   
+   // method that checks if the table in the database exists
+   public static boolean tableExist(Connection conn, String tableName) throws SQLException {
+	    DatabaseMetaData meta = conn.getMetaData(); 
+	    ResultSet res = meta.getTables(null, null, tableName, null); 
+	    while(res.next()){
+	    	return tableExists = true;
+	    }
+	    return tableExists = false;
+	    
+	}
+   
    public static void main(String[] args) {
    Connection conn = null;
    Statement stmt = null;
@@ -32,12 +44,12 @@ public class sampleDatabase {
       tableExists = tableExist(conn, UserInfo);
       
       //if table exists printout this
-      if (tableExists = false){
+      if (tableExists == false){
       System.out.println("Table is already created.");
       }
       
       //if table does not exist do this
-      if (tableExists = true){
+      if (tableExists == true){
     	  String sql = "CREATE TABLE UserInfo " +
                   "(id TIMESTAMP not NULL, " +
                   " user_name VARCHAR(255), " + 
@@ -45,54 +57,40 @@ public class sampleDatabase {
                   " PRIMARY KEY ( id ))"; 
 
       stmt.executeUpdate(sql);
-      System.out.println("Created table in given database...");
+      System.out.println("Created table in given database.");
       }
       
       
-   }
-     catch (SQLException sqlException) {
-       if (sqlException.getErrorCode() == 1007) {
-           // Database already exists error
-           System.out.println("Database is already created");
-       } else {
-           // Some other problems, e.g. Server down, no permission, etc
-           sqlException.printStackTrace();
-       }
-   } catch (ClassNotFoundException e) {
-       // No driver class found!
-   }
+	  }
+	  catch (SQLException sqlException) {
+	       if (sqlException.getErrorCode() == 1007) {
+	           // Database already exists error
+	           System.out.println("Database is already created");
+	       } else {
+	           // Some other problems, e.g. Server down, no permission, etc
+	           sqlException.printStackTrace();
+	       }
+	   } catch (ClassNotFoundException e) {
+	       // No driver class found!
+	   }catch(Exception e){
+	      //Handle errors for Class.forName
+	      e.printStackTrace();
+	   }finally{
+	      //finally block used to close resources
+	      try{
+	         if(stmt!=null)
+	            conn.close();
+	      }catch(SQLException se){
+	      }// do nothing
+	      try{
+	         if(conn!=null)
+	            conn.close();
+	      }catch(SQLException se){
+	         se.printStackTrace();
+	      }//end finally try
+	   }//end try
+	   System.out.println("Goodbye!");
+	}//end main
+	   
    
-   catch(Exception e){
-      //Handle errors for Class.forName
-      e.printStackTrace();
-   }finally{
-      //finally block used to close resources
-      try{
-         if(stmt!=null)
-            conn.close();
-      }catch(SQLException se){
-      }// do nothing
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }//end finally try
-   }//end try
-   System.out.println("Goodbye!");
-}//end main
-   
-   public static boolean tableExist(Connection conn, String tableName) throws SQLException {
-	    boolean tExists = false;
-	    try (ResultSet rs = conn.getMetaData().getTables(null, null, tableName, null)) {
-	        while (rs.next()) { 
-	            String tName = rs.getString("UserInfo");
-	            if (tName != null && tName.equals(tableName)) {
-	                tExists = true;
-	                break;
-	            }
-	        }
-	    }
-	    return tExists;
-	}
 }//end sampleDatabase
